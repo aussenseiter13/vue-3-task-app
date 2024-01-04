f04274ab60f26db7e052dd55c73b3b28670db760
aussenseiter13 <Carefully5693@proton.me>
2024-01-04 12:59:14 +0100

    Update: TaskList.vue, TasItem.vue Comment: Modified componendts in order to start working od adding items to list

--- src/components/Tasks/TaskItem.vue
+++ src/components/Tasks/TaskItem.vue
@@ -5,7 +5,13 @@ defineProps({
 </script>
 
 <template>
-    <li>
-        {{ task.name }}
+    <li class="bg-gray-800 flex gap-4 justify-between px-4 py-2">
+        <p>
+            {{ task.name }}
+        </p>
+        <p
+            v-text="task.completed ? 'Finished' : 'Not finished'"
+            :class="task.completed ? 'text-teal-500' : 'text-red-500'"
+        ></p>
     </li>
 </template>
--- src/components/Tasks/TaskList.vue
+++ src/components/Tasks/TaskList.vue
@@ -1,17 +1,64 @@
 <script setup>
 import TaskItem from "@/components/Tasks/TaskItem.vue";
+import { ref } from "vue";
 
-let tasks = [
+let tasks = ref([
     {
         id: 1,
         name: "Grab Launch",
         completed: true,
     },
-];
+    {
+        id: 2,
+        name: "Practice C++",
+        completed: false,
+    },
+]);
+
+let newTask = {
+    name: ref(""),
+    completed: ref(false),
+};
+
+function addTask() {}
 </script>
 
 <template>
-    <ul>
-        <TaskItem v-for="task in tasks" :key="task.id" :task="task"></TaskItem>
-    </ul>
+    <div class="flex flex-col gap-8">
+        <form
+            @submit.prevent="addTask"
+            class="bg-slate-800 flex flex-col gap-6 px-4 py-4 border border-slate-700 rounded-md text-slate-300"
+        >
+            <div class="flex flex-col gap-2">
+                <label for="" class="">Task name</label>
+                <input
+                    v-model="newTask.name.value"
+                    type="text"
+                    class="bg-slate-700"
+                    placeholder="Example name"
+                />
+            </div>
+            <div class="flex flex-row-reverse justify-end gap-2 items-center">
+                <label for="">Completed</label>
+                <input
+                    type="checkbox"
+                    v-model="newTask.completed"
+                    :checked="newTask.completed"
+                />
+            </div>
+            <button
+                :disabled="newTask.name.value.length ? 0 : !0"
+                class="bg-slate-700 py-2 rounded-md font-bold text-md hover:bg-teal-700 disabled:bg-slate-700 disabled:text-slate-700 disabled:cursor-not-allowed"
+            >
+                Create task
+            </button>
+        </form>
+        <ul class="flex flex-col gap-4">
+            <TaskItem
+                v-for="task in tasks"
+                :key="task.id"
+                :task="task"
+            ></TaskItem>
+        </ul>
+    </div>
 </template>
